19b21f363cbf27e4968303fe1c44a62a71b1d8c8
Lin Ying-Hsuan <wish7912@gmail.com>
2018-07-16 00:47:43 +0800

    pc version

--- index.js
+++ index.js
@@ -2,3 +2,19 @@ $('#fullpage').fullpage({
     sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
     licenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
   });
+
+$('html').bind('mousewheel DOMMouseScroll', function() {
+  var activeSection = fullpage_api.getActiveSection();
+  var isFirstOrLast = activeSection.isFirst || activeSection.isLast;
+  if (isFirstOrLast) {
+    $('.header').addClass('hide');
+  } else {
+    $('.header').removeClass('hide');
+  }
+});
+$('.arrow').on('click', function() {
+  fullpage_api.moveSectionDown();
+})
+$('.gototop').on('click', function() {
+  fullpage_api.moveTo(1);
+})
